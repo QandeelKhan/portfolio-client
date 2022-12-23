87a6207d898e7c310793084926ba2bbcf8350813
Qandeel Khan <com.qandeelhaider@gmail.com>
2022-12-23 21:58:19 +0500

    we made it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <>hello react</>;
+    return (
+        <>
+            hello react, <h1>We made it</h1>
+        </>
+    );
 }
 
 export default App;
