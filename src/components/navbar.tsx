88e76493ed2c7a1f62aaae59b708f8ed5a4e074c
Qandeel Khan <com.qandeelhaider@gmail.com>
2022-12-24 21:25:11 +0500

    added hell in the react app
    1-added changfes
    2- removed changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const navbar = (props: string | boolean) => {
-    return <div>navbar {props}</div>;
+    return <div>navbar hello {props}</div>;
 };
 
 export default navbar;
