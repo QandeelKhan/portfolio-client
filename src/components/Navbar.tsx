09c9483c676154825fe83c3348e90b495407758e
usama-mahmood <darindahp@gmail.com>
2022-12-24 22:42:54 +0500

    saved

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 
 const Navbar = (props: string | boolean) => {
-    return <div>navbar hello {props}</div>;
+    return(
+        <>
+        <div>navbar hello {props} 
+        </div>
+        <h1>my changes</h1>
+        </>
+    ) 
 };
 
 export default Navbar;
