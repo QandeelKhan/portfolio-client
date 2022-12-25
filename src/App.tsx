b81230b31680cac2b94ad010a4b6b8ed82a02084
Qandeel Khan <com.qandeelhaider@gmail.com>
2022-12-25 22:29:04 +0500

    button almost complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import { Button } from "./components/Button";
 function App() {
     return (
         <>
-            hello react, <h1>We made it</h1>
-            <Button/>
-
+            <Button />
         </>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./button.css";
+import HouseOutlinedIcon from "@mui/icons-material/HouseOutlined";
+import homeIcon from "./homeIcon.svg";
+import SvgHomeIcon from "./icons/HomeIcon";
 
 export const Button = () => {
-  return (
-    <div><h3>button added</h3></div>
-  )
-}
+    return (
+        <div className="nav-btn">
+            {/* <HouseOutlinedIcon fontSize="small" className="nav-icon" /> */}
+            {/* <span className="nav-icon">
+                <img src={homeIcon} alt="mySvgImage" />
+            </span> */}
+            <span className="nav-icon">
+                <SvgHomeIcon />
+            </span>
+            <span className="lnr lnr-home"></span>
+            <a href="#home" className="nav-btn-link">
+                Home
+            </a>
+        </div>
+    );
+};
