672371f16e485f3a6de53d4e320a1dea6f1829fb
Qandeel Khan <com.qandeelhaider@gmail.com>
2023-01-07 21:48:24 +0500

    commit button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,12 @@ import React, { ReactNode } from "react";
 import "./button.css";
 import HomeIcon from "./icons/HomeIcon";
 
-export const Button = (props: any) => {
+export const Button = ({ ButtonIcon, buttonTitle, buttonHref }: any) => {
     return (
         <div className="nav-btn">
-            <a href="#home" className="nav-btn-link nav-icon">
-                {props.children}
-            </a>
+            {ButtonIcon}
+            <a href={buttonHref} className="nav-btn-link"></a>
+            {buttonTitle}
         </div>
     );
 };
