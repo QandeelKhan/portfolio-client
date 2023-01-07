408360b2fc3c75935f684184afaa38beea0a9e7b
usama-mahmood <darindahp@gmail.com>
2023-01-07 23:44:01 +0500

    auto commit by py script

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,43 +6,48 @@ import { Button } from "./Button";
 const Navbar: React.FC = (props: any) => {
     return (
         <>
-            <div className="sidebar-main">
-                <div className="image-area">
-                    <img src="profile-img.jpg" alt="profile-img" />
-                    <div className="person-name">
-                        <span>QANDEEL KHAN</span>
+            <div className="main-of-main">
+                <div className="sidebar-main">
+                    <div className="image-area">
+                        <img src="profile-img.jpg" alt="profile-img" />
+                        <div className="person-name">
+                            <span>QANDEEL KHAN</span>
+                        </div>
+                    </div>
+                    <div className="menubar-area">
+                        <ul>
+                            {MenuItems.map((item, index) => {
+                                return (
+                                    <li>
+                                        <Button
+                                            NavBtnIcon={item.icon}
+                                            NavBtnHref={item.path}
+                                            className={item.cName}
+                                            NavBtnTitle={item.title}
+                                        ></Button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <div className="footer-area">
+                        <div className="footer-icons">
+                            <i className="fa-brands fa-square-facebook icons-social"></i>
+                            <i className="fa-brands fa-square-twitter icons-social"></i>
+                            <i className="fa-brands fa-square-youtube icons-social"></i>
+                            <i className="fa-brands fa-linkedin icons-social"></i>
+                            <i className="fa-brands fa-square-dribbble icons-social"></i>
+                        </div>
+                        <div className="footer-text">
+                            <span>
+                                2022 © Cosmos-Themes.
+                                <br /> All Right Reserved.
+                            </span>
+                        </div>
                     </div>
                 </div>
-                <div className="menubar-area">
-                    <ul>
-                        {MenuItems.map((item, index) => {
-                            return (
-                                <li>
-                                    <Button
-                                        NavBtnIcon={item.icon}
-                                        NavBtnHref={item.path}
-                                        className={item.cName}
-                                        NavBtnTitle={item.title}
-                                    ></Button>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-                <div className="footer-area">
-                    <div className="footer-icons">
-                        <i className="fa-brands fa-square-facebook icons-social"></i>
-                        <i className="fa-brands fa-square-twitter icons-social"></i>
-                        <i className="fa-brands fa-square-youtube icons-social"></i>
-                        <i className="fa-brands fa-linkedin icons-social"></i>
-                        <i className="fa-brands fa-square-dribbble icons-social"></i>
-                    </div>
-                    <div className="footer-text">
-                        <span>
-                            2022 © Cosmos-Themes.
-                            <br /> All Right Reserved.
-                        </span>
-                    </div>
+                <div className="main-image-area">
+                    <img src="home-bg.jpg" alt="main-bg" />
                 </div>
             </div>
         </>
