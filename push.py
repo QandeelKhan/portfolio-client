cb7a903e0082cb451da189865e592ab0827c2830
Qandeel Khan <com.qandeelhaider@gmail.com>
2023-01-07 16:55:47 +0500

    auto commit by py script

--- push.py
+++ push.py
@@ -1,4 +1,4 @@
 import subprocess
-subprocess.run(["git", "add", "src/components"])
+subprocess.run(["git", "add", "."])
 subprocess.run(["git", "commit", "-m", "auto commit by py script"])
 subprocess.run(["git", "push"])
