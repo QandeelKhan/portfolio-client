3baa290dbed9693091dd42e2a2034cafb609fefe
usama-mahmood <darindahp@gmail.com>
2023-01-09 16:21:08 +0500

    auto commit by py script

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -114,7 +114,7 @@ const About: React.FC = (props: any) => {
                     {/* service area */}
                     <ServicesArea />
                     {/* Clients Slider */}
-                    {/* <ClientsSlide /> */}
+                    {/* <ClientsSlide / > */}
                 </div>
             </div>
         </section>
