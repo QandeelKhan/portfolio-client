0bbf12be96af8f6826a1cc36498d286461c59256
usama-mahmood <darindahp@gmail.com>
2023-01-10 23:37:42 +0500

    auto commit by py script

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -19,11 +19,11 @@ const Location: React.FC = (props) => {
             </div>
             <div className="map">
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.5944076627347!2d72.7001572143763!3d32.053168127847584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3921780215aca161%3A0xf50464012fbae768!2sGovt%20College%20Of%20Commerce%20Sargodha!5e0!3m2!1sen!2s!4v1673373054081!5m2!1sen!2s"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14143138.321925042!2d59.96986053030338!3d30.067981477326814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38db52d2f8fd751f%3A0x46b7a1f7e614925c!2sPakistan!5e0!3m2!1sen!2s!4v1673374249560!5m2!1sen!2s"
                     width="100%"
                     height="300"
                     // style="border:0;"
-                    // allowfullscreen={""}
+                    // allowfullscreen=""
                     loading="lazy"
                     // referrerpolicy="no-referrer-when-downgrade"
                 ></iframe>
