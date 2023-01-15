0e63312ad9d57069c48efb60c3b2be5acf1dccd6
usama-mahmood <darindahp@gmail.com>
2023-01-15 16:54:15 +0500

    auto commit by py script

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -7,10 +7,8 @@ const Blog = () => {
         <>
             <ResumeHeader iconComponent={<AboutUserIcon />} pageName="Blog.">
                 <h1 className="under-construction">
-                    “We’re working hard to make this page available for you on
-                    20th friday 2023. You will then find the blog page up and
-                    running! Until then we look forward to seeing you on our
-                    social media or you can directly send an email us.”
+                    <span>UNDER </span>
+                    <span className="construction">CONSTRUCTION...</span>
                 </h1>
             </ResumeHeader>
         </>
