92ab7ac552f741b2fb2212f4928cacc17d101bda
Qandeel Khan <com.qandeelhaider@gmail.com>
2023-01-18 22:19:00 +0500

    auto commit by py script

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import PortfolioBriefcase from "../components/icons/PortfolioBriefcase";
 import PortfolioProject from "./PortfolioProject";
 import "./Portfolio.css";
+import PortfolioDetail from "./PortfolioDetail";
 
 const Portfolio = () => {
     return (
-        <section id="resume-section">
-            <div className="about-container">
-                <div className="main-about-area">
-                    <div className="about-head-logo-area">
-                        <h1 className="aboutme-heading">Portfolio.</h1>
-                        <PortfolioBriefcase />
+        <>
+            <PortfolioDetail />
+            <section id="resume-section">
+                <div className="about-container">
+                    <div className="main-about-area">
+                        <div className="about-head-logo-area">
+                            <h1 className="aboutme-heading">Portfolio.</h1>
+                            <PortfolioBriefcase />
+                        </div>
+                        <PortfolioProject />
                     </div>
-                    <PortfolioProject />
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
