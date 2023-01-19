49bd7f9ba1c847d54bd69d929e49bdc9531de39e
Qandeel Khan <com.qandeelhaider@gmail.com>
2023-01-19 19:55:02 +0500

    auto commit by py script

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,28 @@ import "./App.css";
 import SideNav from "./components/SideNav";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import BlogDetail from "./pages/BlogDetail";
-import PortfolioDetail from "./pages/PortfolioDetail";
 import Layout from "./pages/Layout";
-import { useEffect } from "react";
 
 function App() {
     return (
         <>
             <Router>
                 <Routes>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/resume" element={<Resume />} />
-                    <Route path="/portfolio" element={<Portfolio />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/blog-detail" element={<BlogDetail />} />
-                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/" element={<Layout />}>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/resume" element={<Resume />} />
+                        <Route path="/portfolio" element={<Portfolio />} />
+                        <Route path="/blog" element={<Blog />} />
+                        <Route path="/blog-detail" element={<BlogDetail />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Route>
                 </Routes>
             </Router>
         </>
