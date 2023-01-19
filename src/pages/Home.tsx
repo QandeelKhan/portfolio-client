d582da6a99ced9251302224bfe46dbe4e0321f76
usama-mahmood <darindahp@gmail.com>
2023-01-19 20:15:57 +0500

    auto commit by py script

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,14 @@ function Home() {
     return (
         <section id="home" className="home-container">
             <div className="main-image-area"></div>
-            {/* <div className="banner-text">
+            <div className="banner-text">
                 <span className="banner-qandeel">QANDEEL</span>
                 <span className="banner-khan">KHAN</span>
                 <div className="animation-text">
-                    <span className="banner-iam"></span>
-                    <span className="banner-iam"></span>
+                    <span className="banner-iam">i am designer</span>
+                    {/* <span className="banner-iam"></span> */}
                 </div>
-            </div> */}
+            </div>
         </section>
     );
 }
