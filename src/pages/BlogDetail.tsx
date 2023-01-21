dd44f746a5e33130f1cebe80408a8456152f7dbd
usama-mahmood <darindahp@gmail.com>
2023-01-21 17:10:39 +0500

    auto commit by py script

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -331,25 +331,55 @@ const BlogDetail = () => {
                                     {/* comment area starts here */}
                                     <div className="blog-detail-comment-div">
                                         <h3>Write a Comment</h3>
-                                        <div className="comment-area">
-                                            <div className="field-holder ">
-                                                <input
-                                                    type="text"
-                                                    name="user_name"
-                                                    className="form-input"
-                                                    required
-                                                />
-                                                <label>Name</label>
+                                        <div className="comment-write">
+                                            <div className="name-and-email">
+                                                <div className="input-group ">
+                                                    <input
+                                                        type="text"
+                                                        name="user_name"
+                                                        className="form-input"
+                                                        required
+                                                    />
+                                                    <label>Name</label>
+                                                </div>
+                                                <div className="input-group">
+                                                    <input
+                                                        type="email"
+                                                        name="user_email"
+                                                        className="form-input"
+                                                        required
+                                                    />
+                                                    <label>Email</label>
+                                                </div>
                                             </div>
-                                            <div className="field-holder">
-                                                <input
-                                                    type="email"
-                                                    name="user_email"
+                                            <div className="input-group input-message">
+                                                <textarea
                                                     required
+                                                    name="message"
                                                 />
-                                                <label>Email</label>
+                                                <label>Your Comment</label>
                                             </div>
                                         </div>
+                                        <div className="checkbox">
+                                            <input
+                                                type="checkbox"
+                                                // name="user_email"
+                                                className="checkbox"
+                                                // required
+                                            />
+                                            <label>
+                                                Save my name, email, and website
+                                                in this browser for the next
+                                                time I comment.
+                                            </label>
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            className="send-messsage-btn"
+                                            value="Send Message"
+                                        >
+                                            POST COMMENT
+                                        </button>
                                     </div>
                                 </div>
                                 {/* comment section ends here */}
