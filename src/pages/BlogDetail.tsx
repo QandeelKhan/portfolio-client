0b1691f03373ec7d8a7503d581b592ae5e5caabf
usama-mahmood <darindahp@gmail.com>
2023-01-21 20:05:08 +0500

    auto commit by py script

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -388,6 +388,54 @@ const BlogDetail = () => {
                     </div>
                 </div>
             </div>
+            {/* // right sidebar starts here */}
+            <div className="main-side-container">
+                <div className="side-content">
+                    <div className="search-div">
+                        <input type="text" placeholder="Search..." />
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <h3>Recent Posts</h3>
+                    <div className="post-points">
+                        <a href="/">
+                            <span>Best Way To Design</span>
+                        </a>
+                        <a href="/">
+                            <span>Things I Need To Get Started</span>
+                        </a>
+                        <a href="/">
+                            <span>My Current Workspace</span>
+                        </a>
+                        <a href="/">
+                            <span>Best Place To Travel This Summer</span>
+                        </a>
+                        <a href="/">
+                            <span>Latest UI Trends To Inspired From</span>
+                        </a>
+                    </div>
+                    <h3>Recent Comments</h3>
+                    <div className="post-points">
+                        <a href="/">
+                            <span>Best Way To Design</span>
+                        </a>
+                        <a href="/">
+                            <span>Things I Need To Get Started</span>
+                        </a>
+                        <a href="/">
+                            <span>My Current Workspace</span>
+                        </a>
+                        <a href="/">
+                            <span>Best Place To Travel This Summer</span>
+                        </a>
+                        <a href="/">
+                            <span>Latest UI Trends To Inspired From</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="arrow-div">
+                    <i className="fa-solid fa-arrow-left"></i>
+                </div>
+            </div>
         </div>
     );
 };
