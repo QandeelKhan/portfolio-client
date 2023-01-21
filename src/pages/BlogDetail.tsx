345ae55c7c51ad7244fab012409c9da148fbd96e
usama-mahmood <darindahp@gmail.com>
2023-01-21 20:17:21 +0500

    auto commit by py script

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import BlogDetailSidebar from "../components/BlogDetailSidebar";
 import "./blog-detail.css";
 
 const BlogDetail = () => {
@@ -323,20 +324,75 @@ const BlogDetail = () => {
                                             </div>
                                             <div className="comment-para">
                                                 A component that allows for easy
-                                                creation of menu item, quickly
+                                                creation of menu items, quickly
                                                 creating paragraphs of “Lorem
                                                 Ipsum” and pictures with custom
                                                 sizes.
                                             </div>
                                         </div>
                                     </div>
-                                    {/* comment section starts here */}
+                                    {/* comment area starts here */}
+                                    <div className="blog-detail-comment-div">
+                                        <h3>Write a Comment</h3>
+                                        <div className="comment-write">
+                                            <div className="name-and-email">
+                                                <div className="input-group ">
+                                                    <input
+                                                        type="text"
+                                                        name="user_name"
+                                                        className="form-input"
+                                                        required
+                                                    />
+                                                    <label>Name</label>
+                                                </div>
+                                                <div className="input-group">
+                                                    <input
+                                                        type="email"
+                                                        name="user_email"
+                                                        className="form-input"
+                                                        required
+                                                    />
+                                                    <label>Email</label>
+                                                </div>
+                                            </div>
+                                            <div className="input-group input-message">
+                                                <textarea
+                                                    required
+                                                    name="message"
+                                                />
+                                                <label>Your Comment</label>
+                                            </div>
+                                        </div>
+                                        <div className="checkbox">
+                                            <input
+                                                type="checkbox"
+                                                // name="user_email"
+                                                className="checkbox"
+                                                // required
+                                            />
+                                            <label>
+                                                Save my name, email, and website
+                                                in this browser for the next
+                                                time I comment.
+                                            </label>
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            className="send-messsage-btn"
+                                            value="Send Message"
+                                        >
+                                            POST COMMENT
+                                        </button>
+                                    </div>
                                 </div>
+                                {/* comment section ends here */}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {/* // right sidebar starts here */}
+            <BlogDetailSidebar />
         </div>
     );
 };
