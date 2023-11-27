65a064fa72e3f214890addae5bbdda59c073e2da
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-11-27 22:58:48 +0500

    auto commit by py script

--- src/components/FancyBorder.tsx
+++ src/components/FancyBorder.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function FancyBorder() {
     return (
         <div className="border-container">
-            s<div className="border-pole"></div>
+            <div className="border-pole"></div>
             <div className="bordered-content"></div>
         </div>
     );
