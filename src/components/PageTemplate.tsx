15dac6b58378610c5378e8c755e1fcc637cd681e
Qandeel Haider Khan <com.qandeelhaider@gmail.com>
2026-09-22 23:39:39 +0500

    ui fixes on light mode

--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -2,7 +2,7 @@ import "./css/page-template.css";
 const PageTemplate = (props: any) => {
     return (
         <div className="about-container">
-            <div className="main-about-area">
+            <div className="page-content-area">
                 <div className="about-head-logo-area">
                     <h1 className="aboutme-heading">{props.pageName}</h1>
                     {props.iconComponent}
